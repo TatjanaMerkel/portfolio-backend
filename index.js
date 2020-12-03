cb5fc186dcbc59c133f130e147dccbe3939aea57
Tatjana Merkel <tatjana_merkel@outlook.com>
2020-12-04 00:17:38 +0100

    [OS-1] style(routes): Format index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/products/', (req, res) => {
-    res.json({a: 'abc', b: 12})
-})
+app.get("/products/", (req, res) => {
+  res.json({ a: "abc", b: 12 });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
