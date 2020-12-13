ff7aa81e9aa80d25033e63d45cfd446677ad90a9
Tatjana Merkel <tatjana_merkel@outlook.com>
2020-12-13 23:27:31 +0100

    Get mysql demo code running

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const cors = require('cors');
 const express = require("express");
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '12345678',
+  database : 'my_db'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
 
 const app = express();
 const port = 3001;
