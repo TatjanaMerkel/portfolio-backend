9841af8ec9e656708f504ebc11b783a318a3b9dc
Tatjana Merkel <tatjana_merkel@outlook.com>
2020-12-14 00:40:03 +0100

    [OS-1] feat(routes): Get products from DB

--- index.js
+++ index.js
@@ -1,69 +1,38 @@
-const cors = require('cors');
+const cors = require("cors");
 const express = require("express");
-var mysql = require('mysql');
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '12345678',
-  database : 'my_db'
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "12345678",
+  database: "portfolio_db",
 });
- 
-connection.connect();
- 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
- 
-connection.end();
 
 const app = express();
 const port = 3001;
 
 app.use(cors());
 
-products = [
-  {
-    id: 1,
-    category: 'fruits',
-    title: "Papaya",
-    description: "Papayas are great",
-    image:
-      "https://5.imimg.com/data5/GG/CC/VN/SELLER-47385627/fresh-high-quality-papaya-500x500.jpg",
-  },
-  {
-    id: 2,
-    category: 'fruits',
-    title: "Banana",
-    description: "Tati is great",
-    image:
-      "https://images-na.ssl-images-amazon.com/images/I/61fZ%2BYAYGaL._SX679_.jpg",
-  },
-  {
-    id: 3,
-    category: 'fruits',
-    title: "Blueberry",
-    description: "Blueberries are great",
-    image:
-      "https://5.imimg.com/data5/RI/NH/MY-50344271/natural-fresh-blueberry-500x500.jpg",
-  },
-  {
-    id: 4,
-    category: 'vegetables',
-    title: "Cucumber",
-    description: "Cucumbers are great",
-    image:
-      "https://images.heb.com/is/image/HEBGrocery/000319432",
-  },
-];
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/products/", (req, res) => {
-  res.json(products);
+  connection.connect();
+
+  connection.query(
+    "SELECT category, name, description, image FROM products",
+    function (error, results, fields) {
+      if (error) throw error;
+
+      console.log("The solution is: ", results);
+
+      res.json(results);
+    }
+  );
+
+  connection.end();
 });
 
 app.listen(port, () => {
