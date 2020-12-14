1d978811330adbfcc6fe1ecc1c39aff50ce47bec
Tatjana Merkel <tatjana_merkel@outlook.com>
2020-12-14 02:37:38 +0100

    [OS-2] feat(routes): Create new connection

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const cors = require("cors");
 const express = require("express");
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "12345678",
-  database: "portfolio_db",
-});
-
 const app = express();
 const port = 3001;
 
@@ -19,6 +12,14 @@ app.get("/", (req, res) => {
 });
 
 app.get("/products/", (req, res) => {
+
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "12345678",
+    database: "portfolio_db",
+  });
+  
   connection.connect();
 
   connection.query(
